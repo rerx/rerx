15e8fcda9d2a36041f1ea97925dde92f7b56ae79
Jas Chen <jas228@gmail.com>
2016-07-23 16:45:21 +0800

    Update example

--- examples/counter/src/index.js
+++ examples/counter/src/index.js
@@ -1,18 +1,29 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render, unmountComponentAtNode } from 'react-dom';
 import { select, run } from 'rerx/core';
 import { Counter } from './Counter';
 
 const mountNode = document.getElementById("root");
+const component$ = select(components => components);
 
-// Component events are observable.
+
+// Mounting components are observable.
+component$.subscribe(components => {
+  console.info(components);
+});
+
+// Events are observable.
 select('#my-counter', 'incrementClick').subscribe(e => {
   console.log(e);
 });
 
-// Component state is observable.
+// States are observable.
 select('#my-counter', 'count$').subscribe(count => {
   console.log(count);
 });
 
-ReactDOM.render(<Counter id="my-counter" /> , mountNode, run);
+
+render(<Counter id="my-counter" /> , mountNode, run);
+
+// Try calling unmount() from the console.
+window.unmount = () => unmountComponentAtNode(mountNode);
